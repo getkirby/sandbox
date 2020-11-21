e7f5fa70378962c17884637b187183eb03ce8995
Lukas Bestle <mail@lukasbestle.com>
2020-11-21 17:38:49 +0100

    Add router script

---
+++ router.php
@@ -0,0 +1,12 @@
+<?php
+
+$root = __DIR__ . '/public';
+
+// https://yourdomain.com/media/super/nice.jpg
+if (file_exists($root . '/' . $_SERVER['SCRIPT_NAME'])) {
+    return false; // serve the requested resource as-is.
+}
+
+$_SERVER['SCRIPT_NAME'] = str_replace($_SERVER['DOCUMENT_ROOT'], '', $index = $root . '/index.php');
+
+include $index;
