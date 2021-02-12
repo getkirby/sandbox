0c33ecd7a7b0a8a6451f9d9d219a5366159635db
Bastian Allgeier <mail@bastianallgeier.com>
2021-02-12 10:27:26 +0100

    Remove outdated builder environment

--- environments/builder/site/snippets/blocks/bodytext.php
+++
@@ -1,3 +0,0 @@
-<section class="bodytext">
-  <?= $data->text()->kt() ?>
-</section>
--- environments/builder/site/templates/default.php
+++
@@ -1,3 +0,0 @@
-<?php foreach ($page->builder()->toBuilderBlocks() as $block): ?>
-<?php snippet('blocks/' . $block->_key(), ['data' => $block]) ?>
-<?php endforeach ?>
