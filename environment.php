cf54d725abbebeddf50fa4af88b96b099c027653
Lukas Bestle <mail@lukasbestle.com>
2022-11-22 22:16:58 +0100

    Convert indentation to tabs

--- environment.php
+++ environment.php
@@ -5,201 +5,199 @@ use Kirby\Filesystem\F;
 
 class Environment
 {
+	/**
+	 * Returns the name of the currently active environment
+	 */
+	public static function active(): string
+	{
+		return F::read(__DIR__ . '/public/.environment');
+	}
 
-    /**
-     * Returns the name of the currently active environment
-     */
-    public static function active(): string
-    {
-        return F::read(__DIR__ . '/public/.environment');
-    }
+	/**
+	 * Authenticates as the given user without login
+	 */
+	public static function auth(string $username): bool
+	{
+		if ($user = kirby()->user($username)) {
+			$user->loginPasswordless();
+			return true;
+		}
 
-    /**
-     * Authenticates as the given user without login
-     */
-    public static function auth(string $username): bool
-    {
-        if ($user = kirby()->user($username)) {
-            $user->loginPasswordless();
-            return true;
-        }
+		throw new Exception('The user could not be found');
+	}
 
-        throw new Exception('The user could not be found');
-    }
+	/**
+	 * Deletes an available environment from the `environments` folder
+	 */
+	public static function delete(string $environment): void
+	{
+		if (static::exists($environment) !== true) {
+			throw new Exception('The environment could not be found');
+		}
 
-    /**
-     * Deletes an available environment from the `environments` folder
-     */
-    public static function delete(string $environment): void
-    {
-        if (static::exists($environment) !== true) {
-            throw new Exception('The environment could not be found');
-        }
+		Dir::remove(static::root($environment));
+	}
 
-        Dir::remove(static::root($environment));
-    }
+	/**
+	 * Returns a list of all available environments with an `active`
+	 * marker for the current environment
+	 */
+	public static function envs(): array
+	{
+		$envs = [];
 
-    /**
-     * Returns a list of all available environments with an `active`
-     * marker for the current environment
-     */
-    public static function envs(): array
-    {
-        $envs = [];
+		$activeEnv = static::active();
 
-        $activeEnv = static::active();
+		foreach (Dir::read(__DIR__ . '/environments') as $env) {
+			$root = static::root($env);
 
-        foreach (Dir::read(__DIR__ . '/environments') as $env) {
-            $root = static::root($env);
+			$envs[] = [
+				'name'   => $env,
+				'active' => $env === $activeEnv
+			];
+		}
 
-            $envs[] = [
-                'name'   => $env,
-                'active' => $env === $activeEnv
-            ];
-        }
+		return $envs;
+	}
 
-        return $envs;
-    }
+	/**
+	 * Copies an environment into the `public` dir
+	 * and prepares it for use
+	 */
+	public static function install(string $environment): bool
+	{
+		if (static::exists($environment) !== true) {
+			throw new Exception('The environment could not be found');
+		}
 
-    /**
-     * Copies an environment into the `public` dir
-     * and prepares it for use
-     */
-    public static function install(string $environment): bool
-    {
-        if (static::exists($environment) !== true) {
-            throw new Exception('The environment could not be found');
-        }
+		$root   = static::root($environment);
+		$public = __DIR__ . '/public';
 
-        $root   = static::root($environment);
-        $public = __DIR__ . '/public';
+		// remove previous environment
+		static::remove();
 
-        // remove previous environment
-        static::remove();
+		$directories = [
+			'assets',
+			'content',
+			'site'
+		];
 
-        $directories = [
-            'assets',
-            'content',
-            'site'
-        ];
+		foreach ($directories as $directory) {
+			if (is_dir($root . '/' . $directory)) {
+				Dir::copy($root . '/' . $directory, $public . '/' . $directory);
+			}
+		}
 
-        foreach ($directories as $directory) {
-            if (is_dir($root . '/' . $directory)) {
-                Dir::copy($root . '/' . $directory, $public . '/' . $directory);
-            }
-        }
+		// copy the global ray plugin
+		Dir::copy(__DIR__ . '/plugins/ray', $public . '/site/plugins/ray');
 
-        // copy the global ray plugin
-        Dir::copy(__DIR__ . '/plugins/ray', $public . '/site/plugins/ray');
+		// remove pre-installed users
+		if (is_dir($public . '/site/accounts') === true) {
+			Dir::remove($public . '/site/accounts');
+		}
 
-        // remove pre-installed users
-        if (is_dir($public . '/site/accounts') === true) {
-            Dir::remove($public . '/site/accounts');
-        }
+		// store the name of the environment for switching later
+		F::write($public . '/.environment', $environment);
 
-        // store the name of the environment for switching later
-        F::write($public . '/.environment', $environment);
+		return true;
+	}
 
-        return true;
-    }
+	/**
+	 * Returns whether an environment exists in the `environments` folder
+	 */
+	public static function exists(string $environment): bool
+	{
+		return is_dir(static::root($environment));
+	}
 
-    /**
-     * Returns whether an environment exists in the `environments` folder
-     */
-    public static function exists(string $environment): bool
-    {
-        return is_dir(static::root($environment));
-    }
+	/**
+	 * Clears the currently active installation in the `public` folder
+	 */
+	public static function remove()
+	{
+		$public = __DIR__ . '/public';
 
-    /**
-     * Clears the currently active installation in the `public` folder
-     */
-    public static function remove()
-    {
-        $public = __DIR__ . '/public';
+		Dir::remove($public . '/assets');
+		Dir::remove($public . '/content');
+		Dir::remove($public . '/media');
+		Dir::remove($public . '/site');
+	}
 
-        Dir::remove($public . '/assets');
-        Dir::remove($public . '/content');
-        Dir::remove($public . '/media');
-        Dir::remove($public . '/site');
-    }
+	/**
+	 * Returns the root to an environment in the `environments` folder
+	 */
+	public static function root(string $environment): string
+	{
+		return __DIR__ . '/environments/' . basename($environment);
+	}
 
-    /**
-     * Returns the root to an environment in the `environments` folder
-     */
-    public static function root(string $environment): string
-    {
-        return __DIR__ . '/environments/' . basename($environment);
-    }
+	/**
+	 * Copies the current installation from the `public` folder to
+	 * a new or existing environment in the `environments` folder
+	 *
+	 * @param string|null $environment Target environment (defaults to current one)
+	 */
+	public static function store(?string $environment = null): bool
+	{
+		$public      = __DIR__ . '/public';
+		$environment = $environment ?? F::read($public . '/.environment');
+		if (!$environment) {
+			return false;
+		}
 
-    /**
-     * Copies the current installation from the `public` folder to
-     * a new or existing environment in the `environments` folder
-     *
-     * @param string|null $environment Target environment (defaults to current one)
-     */
-    public static function store(?string $environment = null): bool
-    {
-        $public      = __DIR__ . '/public';
-        $environment = $environment ?? F::read($public . '/.environment');
-        if (!$environment) {
-            return false;
-        }
+		$root = static::root($environment);
 
-        $root = static::root($environment);
+		// remove the previous state
+		Dir::remove($root);
+		Dir::make($root);
 
-        // remove the previous state
-        Dir::remove($root);
-        Dir::make($root);
+		$directories = [
+			'assets',
+			'content',
+			'site'
+		];
 
-        $directories = [
-            'assets',
-            'content',
-            'site'
-        ];
+		foreach ($directories as $directory) {
+			if (is_dir($public . '/' . $directory)) {
+				Dir::copy($public . '/' . $directory, $root . '/' . $directory);
+			}
+		}
 
-        foreach ($directories as $directory) {
-            if (is_dir($public . '/' . $directory)) {
-                Dir::copy($public . '/' . $directory, $root . '/' . $directory);
-            }
-        }
+		// remove directories that should not be stored
+		Dir::remove($root . '/site/accounts');
+		Dir::remove($root . '/site/cache');
+		Dir::remove($root . '/site/plugins/ray');
+		Dir::remove($root . '/site/sessions');
 
-        // remove directories that should not be stored
-        Dir::remove($root . '/site/accounts');
-        Dir::remove($root . '/site/cache');
-        Dir::remove($root . '/site/plugins/ray');
-        Dir::remove($root . '/site/sessions');
+		// store the name of the environment for switching later
+		F::write($public . '/.environment', $environment);
 
-        // store the name of the environment for switching later
-        F::write($public . '/.environment', $environment);
+		return true;
+	}
 
-        return true;
-    }
+	/**
+	 * Copies a user preset from `accounts` to the `public` folder
+	 */
+	public static function user(string $username): bool
+	{
+		$accounts = __DIR__ . '/accounts';
+		$account  = $accounts . '/' . basename($username);
+		$public   = __DIR__ . '/public';
+		$dest     = $public . '/site/accounts/' . basename($username);
 
-    /**
-     * Copies a user preset from `accounts` to the `public` folder
-     */
-    public static function user(string $username): bool
-    {
-        $accounts = __DIR__ . '/accounts';
-        $account  = $accounts . '/' . basename($username);
-        $public   = __DIR__ . '/public';
-        $dest     = $public . '/site/accounts/' . basename($username);
+		if (is_dir($account) === false) {
+			throw new Exception('The user does not exist');
+		}
 
-        if (is_dir($account) === false) {
-            throw new Exception('The user does not exist');
-        }
+		// remove previous versions of the user
+		Dir::remove($dest);
 
-        // remove previous versions of the user
-        Dir::remove($dest);
+		// create the account folder if it does not exist
+		Dir::make(dirname($dest));
 
-        // create the account folder if it does not exist
-        Dir::make(dirname($dest));
+		// copy the account
+		Dir::copy($account, $dest);
 
-        // copy the account
-        Dir::copy($account, $dest);
-
-        return true;
-    }
-
+		return true;
+	}
 }
--- router.php
+++ router.php
@@ -4,7 +4,7 @@ $root = __DIR__ . '/public';
 
 // https://yourdomain.com/media/super/nice.jpg
 if (file_exists($root . '/' . $_SERVER['SCRIPT_NAME'])) {
-    return false; // serve the requested resource as-is.
+	return false; // serve the requested resource as-is.
 }
 
 $_SERVER['SCRIPT_NAME'] = str_replace($_SERVER['DOCUMENT_ROOT'], '', $index = $root . '/index.php');
