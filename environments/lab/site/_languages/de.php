4f73cb5c2a135a73def7f032aada7e3cc02aac2b
Bastian Allgeier <mail@bastianallgeier.com>
2023-06-22 09:49:50 +0200

    New lab environment

---
+++ environments/lab/site/_languages/de.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+  'code' => 'de',
+  'default' => false,
+  'direction' => 'ltr',
+  'locale' => 'de_DE',
+  'name' => 'Deutsch',
+  'translations' => [
+    'option.a' => 'Variante A',
+    'option.b' => 'Variante B',
+    'option.c' => 'Variante C'
+  ]
+];
---
+++ environments/lab/site/_languages/en.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+  'code' => 'en',
+  'default' => true,
+  'direction' => 'ltr',
+  'locale' => 'en_EN',
+  'name' => 'English',
+  'translations' => [
+    'option.a' => 'Option A',
+    'option.b' => 'Option B',
+    'option.c' => 'Option C'
+  ]
+];
