10b7f59779d896492df0adc5bad2a372f2c1f054
Nico Hoffmann <me@nhoffmann.com>
2023-09-07 21:34:58 +0200

    Ui: examples for `k-pagination`

---
+++ environments/lab/site/ui/pagination/1_layout/index.vue
@@ -0,0 +1,38 @@
+<template>
+	<k-ui-examples>
+		<k-ui-example label="Default">
+			<k-pagination
+				:page="page"
+				:total="total"
+				@paginate="page = $event.page"
+			/>
+		</k-ui-example>
+		<k-ui-example label="Details">
+			<k-pagination
+				:page="page"
+				:total="total"
+				:details="true"
+				@paginate="page = $event.page"
+			/>
+		</k-ui-example>
+		<k-ui-example label="Keys">
+			<k-pagination
+				:page="page"
+				:total="total"
+				:keys="true"
+				@paginate="page = $event.page"
+			/>
+		</k-ui-example>
+	</k-ui-examples>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			page: 1,
+			total: 50,
+		};
+	},
+};
+</script>
