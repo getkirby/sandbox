dc5ad4978193ca04d58afea8ac1d28e8f49b84b3
Bastian Allgeier <mail@bastianallgeier.com>
2023-09-13 13:44:09 +0200

    Better choice examples

--- environments/lab/site/ui/inputs/choice/index.vue
+++ environments/lab/site/ui/inputs/choice/index.vue
@@ -3,12 +3,31 @@
 		<k-ui-example label="Default">
 			<k-choice-input :checked="value" @input="value = $event" />
 		</k-ui-example>
-		<k-ui-example label="Radio">
-			<k-choice-input :checked="value" type="radio" @input="value = $event" />
+		<k-ui-example label="Label">
+			<k-choice-input :checked="value" label="Option" @input="value = $event" />
 		</k-ui-example>
-		<k-ui-example label="Toggle">
+		<k-ui-example label="Label & Info">
 			<k-choice-input
 				:checked="value"
+				info="This is some info text"
+				label="Option"
+				@input="value = $event"
+			/>
+		</k-ui-example>
+		<k-ui-example label="Type: radio">
+			<k-choice-input
+				:checked="value"
+				info="This is some info text"
+				label="Option"
+				type="radio"
+				@input="value = $event"
+			/>
+		</k-ui-example>
+		<k-ui-example label="Variant: toggle">
+			<k-choice-input
+				:checked="value"
+				info="This is some info text"
+				label="Option"
 				variant="toggle"
 				@input="value = $event"
 			/>
@@ -17,6 +36,8 @@
 			<k-choice-input
 				:autofocus="true"
 				:checked="value"
+				info="This is some info text"
+				label="Option"
 				@input="value = $event"
 			/>
 		</k-ui-example>
@@ -24,6 +45,8 @@
 			<k-choice-input
 				:disabled="true"
 				:checked="value"
+				info="This is some info text"
+				label="Option"
 				@input="value = $event"
 			/>
 		</k-ui-example>
