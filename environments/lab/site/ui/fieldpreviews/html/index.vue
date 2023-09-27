184c6c2b59da137fa59908cd976161b02da6e793
Bastian Allgeier <mail@bastianallgeier.com>
2023-09-27 17:39:36 +0200

    New table and fieldpreview examples

--- environments/lab/site/ui/fieldpreviews/html/index.vue
+++ environments/lab/site/ui/fieldpreviews/html/index.vue
@@ -5,6 +5,20 @@
 			label="Default"
 			type="html"
 		/>
+		<k-ui-field-preview-example :value="html" label="Multiline" type="html" />
 		<k-ui-field-preview-example label="Empty" type="html" />
 	</k-ui-examples>
 </template>
+
+<script>
+export default {
+	computed: {
+		html() {
+			return `
+				<p>First <strong>paragraph</strong> with some more <code>HTML</code> to fill the space</p>
+				<p>One more paragraph with <a href="">a link</a></p>
+			`;
+		},
+	},
+};
+</script>
