9eee6b277764adb59a58b1527a08434d964c2ad2
Bastian Allgeier <mail@bastianallgeier.com>
2023-09-27 14:10:58 +0200

    New block examples

--- environments/lab/site/ui/blocks/fields/index.vue
+++ environments/lab/site/ui/blocks/fields/index.vue
@@ -3,5 +3,35 @@
 		<k-ui-example label="Default">
 			<k-block type="fields" />
 		</k-ui-example>
+		<k-ui-example label="Tabs">
+			<k-block type="fields" :fieldset="fieldsetWithTabs" />
+		</k-ui-example>
 	</k-ui-examples>
 </template>
+
+<script>
+export default {
+	computed: {
+		fieldset() {
+			return {
+				name: "Test",
+			};
+		},
+		fieldsetWithTabs() {
+			return {
+				...this.fieldset,
+				tabs: {
+					content: {
+						name: "content",
+						label: "Content",
+					},
+					settings: {
+						name: "settings",
+						label: "Settings",
+					},
+				},
+			};
+		},
+	},
+};
+</script>
