7c1e09a0170253819fb0376a447a1909069c78d3
Bastian Allgeier <mail@bastianallgeier.com>
2023-10-03 18:18:27 +0200

    More UI examples and new docs

--- environments/lab/site/ui/text/5_code/index.vue
+++ environments/lab/site/ui/text/5_code/index.vue
@@ -8,5 +8,10 @@
 				<pre><code>This is a code block</code></pre>
 			</k-text>
 		</k-ui-example>
+		<k-ui-example label="block with language">
+			<k-text>
+				<pre><code class="language-php">if ($page->title() === 'blog') echo 'hello'</code></pre>
+			</k-text>
+		</k-ui-example>
 	</k-ui-examples>
 </template>
