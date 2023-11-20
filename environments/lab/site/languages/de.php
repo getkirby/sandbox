4a7d24466722a3464bbf0fdeecd169b160d5ae6a
Lukas Bestle <mail@lukasbestle.com>
2023-11-20 21:59:35 +0100

    Lab env: Add XSS playground

--- environments/lab/site/languages/de.php
+++ environments/lab/site/languages/de.php
@@ -5,7 +5,7 @@ return [
   'default' => false,
   'direction' => 'ltr',
   'locale' => 'de_DE',
-  'name' => 'Deutsch',
+  'name' => '"><strong style="color: var(--color-red-700)">Malicious Deutsch</strong><img onerror="alert(\'XSS: Language name\')" src="x">',
   'translations' => [
     'option.a' => 'Variante A',
     'option.b' => 'Variante B',
