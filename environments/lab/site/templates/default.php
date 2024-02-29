b53c3abc546718d8cb30c70ef07208b4491e7723
Bastian Allgeier <mail@bastianallgeier.com>
2024-02-29 17:42:08 +0100

    Create simple default template and content folders for home and error

---
+++ environments/lab/site/templates/default.php
@@ -0,0 +1,40 @@
+<style>
+
+* {
+	font-family: system-ui;
+	margin: 0;
+	padding: 0;
+}
+
+body {
+	padding: 1.5rem;
+}
+
+h1 {
+	margin-bottom: 1.5rem;
+}
+
+dt {
+	font-weight: 600;
+}
+
+dd {
+	margin-bottom: 1.5rem;
+}
+
+
+</style>
+
+<h1><?= $page->title() ?></h1>
+
+<dl>
+	<dt>URL</dt>
+	<dd><?= $page->url() ?></dd>
+	<dt>UUID</dt>
+	<dd><?= $page->uuid() ?></dd>
+	<dt>UUID-URL</dt>
+	<dd><?= $page->uuid()->url() ?></dd>
+	<dt>Language</dt>
+	<dd><?= $kirby->language()->code() ?></dd>
+</dl>
+
