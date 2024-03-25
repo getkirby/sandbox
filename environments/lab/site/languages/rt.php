97a31412b28e4233bc38c793c1c752656e9af6b0
Bastian Allgeier <mail@bastianallgeier.com>
2024-03-25 11:17:01 +0100

    Add rtl language

---
+++ environments/lab/site/languages/rt.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'code' => 'rt',
+    'default' => false,
+    'direction' => 'rtl',
+    'locale' => [
+        'LC_ALL' => 'rt'
+    ],
+    'name' => 'RTL Language',
+    'translations' => [
+
+    ],
+    'url' => NULL
+];
