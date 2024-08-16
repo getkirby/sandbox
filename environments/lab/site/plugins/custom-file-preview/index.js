28cf6341e12b1dccb66e0ca348590023d1c37faa
Nico Hoffmann <me@nhoffmann.com>
2024-08-16 11:15:19 +0200

    Fix vertical alignment of custom file preview

--- environments/lab/site/plugins/custom-file-preview/index.js
+++ environments/lab/site/plugins/custom-file-preview/index.js
@@ -8,9 +8,7 @@ panel.plugin("getkirby/custom-file-preview", {
 						<model-viewer :src="url" ar shadow-intensity="1" camera-controls touch-action="pan-y" />
 					</k-file-preview-thumb>
 
-					<figcaption>
-						<k-file-preview-details :details="details" />
-					</figcaption>
+					<k-file-preview-details :details="details" />
 				</figure>
 			`,
 			props: {
