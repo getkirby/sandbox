073b9d1b15612874cb0425b542d542ce825e5700
Nico Hoffmann <me@nhoffmann.com>
2024-09-09 18:42:12 +0200

    Fix custom file preview plugin

--- environments/lab/site/plugins/custom-file-preview/index.js
+++ environments/lab/site/plugins/custom-file-preview/index.js
@@ -3,10 +3,10 @@ panel.plugin("getkirby/custom-file-preview", {
 		"k-glb-file-preview": {
 			template: `
 				<figure class="k-default-file-preview k-glb-file-preview">
-					<k-file-preview-thumb>
+					<k-file-preview-frame>
 						<script type="module" :src="asset"></script>
 						<model-viewer :src="url" ar shadow-intensity="1" camera-controls touch-action="pan-y" />
-					</k-file-preview-thumb>
+					</k-file-preview-frame>
 
 					<k-file-preview-details :details="details" />
 				</figure>
