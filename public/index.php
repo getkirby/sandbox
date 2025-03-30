0191318fc002e7b0ed279ef12f73389bf866d9e6
Lukas Bestle <mail@lukasbestle.com>
2025-03-30 22:03:43 +0200

    Spaces to tabs

--- public/index.php
+++ public/index.php
@@ -6,56 +6,56 @@ require $root . '/kirby/bootstrap.php';
 require $root . '/environment.php';
 
 $kirby = new Kirby([
-    'options' => [
-        'api' => [
-            'csrf' => 'dev'
-        ],
-        'debug' => true,
-        'email' => [
-            'transport' => [
-                'type'     => 'smtp',
-                'host'     => '127.0.0.1',
-                'port'     => 2525,
-                'security' => false,
-                'username' => 'sandbox.test',
-                'password' => null
-            ]
-        ],
-        'panel' => [
-            'dev' => true
-        ]
-    ],
-    'roots' => [
-        'index' => __DIR__
-    ],
-    'routes' => [
-        [
-            'pattern' => '/env/auth/(:any)',
-            'action'  => function ($username) {
-                Environment::auth($username);
+	'options' => [
+		'api' => [
+			'csrf' => 'dev'
+		],
+		'debug' => true,
+		'email' => [
+			'transport' => [
+				'type'     => 'smtp',
+				'host'     => '127.0.0.1',
+				'port'     => 2525,
+				'security' => false,
+				'username' => 'sandbox.test',
+				'password' => null
+			]
+		],
+		'panel' => [
+			'dev' => true
+		]
+	],
+	'roots' => [
+		'index' => __DIR__
+	],
+	'routes' => [
+		[
+			'pattern' => '/env/auth/(:any)',
+			'action'  => function ($username) {
+				Environment::auth($username);
 
-                if (get('panel') !== null) {
-                    return go('panel');
-                }
+				if (get('panel') !== null) {
+					return go('panel');
+				}
 
-                die('The user has been authenticated');
-            }
-        ],
-        [
-            'pattern' => '/env/install/(:any)',
-            'action'  => function (string $env) {
-                Environment::install($env);
-                die('The environment has been installed');
-            }
-        ],
-        [
-            'pattern' => '/env/user/(:any)',
-            'action'  => function (string $username) {
-                Environment::user($username);
-                die('The user has been created');
-            }
-        ]
-    ]
+				die('The user has been authenticated');
+			}
+		],
+		[
+			'pattern' => '/env/install/(:any)',
+			'action'  => function (string $env) {
+				Environment::install($env);
+				die('The environment has been installed');
+			}
+		],
+		[
+			'pattern' => '/env/user/(:any)',
+			'action'  => function (string $username) {
+				Environment::user($username);
+				die('The user has been created');
+			}
+		]
+	]
 ]);
 
 echo $kirby->render();
