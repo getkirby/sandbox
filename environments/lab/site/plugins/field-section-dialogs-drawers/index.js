8dbef09f3ce5940590d736351fc2143a9fafbed4
Nico Hoffmann <me@nhoffmann.com>
2025-09-21 12:25:38 +0200

    fix: Add help text with version support

--- environments/lab/site/plugins/field-section-dialogs-drawers/index.js
+++ environments/lab/site/plugins/field-section-dialogs-drawers/index.js
@@ -2,10 +2,11 @@ panel.plugin("plugins/field-dialogs-drawers", {
 	fields: {
 		"field-with-dialogs-drawers": {
 			props: {
+				label: String,
 				endpoints: Object,
 			},
 			template: `
-				<k-field>
+				<k-field v-bind="$props">
 					<k-button-group>
 						<k-button
 							text="Open dialog"
@@ -29,7 +30,7 @@ panel.plugin("plugins/field-dialogs-drawers", {
 				parent: String,
 			},
 			template: `
-				<k-section label="Section with dialogs and drawers">
+				<k-section v-bind="$props" label="Section with dialogs and drawers">
 					<k-button-group>
 						<k-button
 							text="Open dialog"
@@ -42,6 +43,11 @@ panel.plugin("plugins/field-dialogs-drawers", {
 							@click="$drawer(parent + '/sections/' + name + '/test')"
 						/>
 					</k-button-group>
+					<k-text
+						html="Section dialogs/drawers only work in Kirby 6+"
+						class="k-help"
+						style="margin-top: var(--spacing-2)"
+					/>
 				</k-section>
 			`,
 		},
