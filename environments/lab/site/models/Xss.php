039a10e337a94114e0a2d59e4c4a55954132e6d6
Bastian Allgeier <mail@bastianallgeier.com>
2025-11-13 15:31:22 +0100

    fix: XSS test page

--- environments/lab/site/models/Xss.php
+++ environments/lab/site/models/Xss.php
@@ -7,7 +7,7 @@ class XssPage extends Page
 		$xss = '"><strong style="color: var(--color-red-700)">Malicious string</strong><img onerror="alert(\'XSS: Page title\')" src="x">';
 
 		return [
-			$xss => '#aaa'
+			'#aaa' => $xss
 		];
 	}
 }
