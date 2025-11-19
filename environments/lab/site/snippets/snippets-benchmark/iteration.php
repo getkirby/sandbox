5929eff3dea31827928feda1cdf5c28325ddfb71
Bastian Allgeier <mail@bastianallgeier.com>
2025-11-19 14:32:48 +0100

    feat: New benchmark section for test setups

---
+++ environments/lab/site/snippets/snippets-benchmark/iteration.php
@@ -0,0 +1,1 @@
+<p>Iteration: <?= $iteration ?></p>
---
+++ environments/lab/site/templates/snippets-benchmark.php
@@ -0,0 +1,3 @@
+<?php foreach (range(0,1000) as $iteration): ?>
+<?php snippet('snippets-benchmark/iteration', ['iteration' => $iteration]) ?>
+<?php endforeach ?>
